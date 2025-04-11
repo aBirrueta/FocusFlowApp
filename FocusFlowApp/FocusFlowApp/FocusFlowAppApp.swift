919c90ef54a9b392813944a5aca2a471d1b62728
aBirrueta <alejandrobirru3ta@gmail.com>
2025-04-11 09:27:36 -0700

    created project on Xcode

---
+++ FocusFlowApp/FocusFlowApp/FocusFlowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFlowAppApp.swift
+//  FocusFlowApp
+//
+//  Created by Alejandro Birrueta on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FocusFlowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FocusFlowApp/FocusFlowAppTests/FocusFlowAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFlowAppTests.swift
+//  FocusFlowAppTests
+//
+//  Created by Alejandro Birrueta on 4/11/25.
+//
+
+import Testing
+@testable import FocusFlowApp
+
+struct FocusFlowAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
