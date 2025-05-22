2d6b1f0ffc531589668c944576e3d30d2bada077
aBirrueta <alejandrobirru3ta@gmail.com>
2025-05-22 10:06:30 -0700

    got timer working with start/pause button and reset buttons working

--- FocusFlowApp/FocusFlowApp/PomoTimer/PomoTimerView.swift
+++ FocusFlowApp/FocusFlowApp/PomoTimer/PomoTimerView.swift
@@ -9,22 +9,59 @@ import SwiftUI
 
 struct PomoTimerView: View {
     @State var timeRemaining = 60
-    @State var minutesRemaing = 0.0
-    @State var secondsRemaing = 0
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var isRunning = false
+    @State private var timer: Timer?
     var body: some View {
-        Text("\(minutesRemaing)"+":"+"\(secondsRemaing)")
-            .onReceive(timer) { _ in
-                secondsRemaing = timeRemaining % timeRemaining
-                if timeRemaining > 0 {
-                    timeRemaining -= 1
-                }
-                if timeRemaining >= 60{
-                    minutesRemaing = Double(timeRemaining/60)
-                }
+        Text("PomoTimer")
+            .font(.title)
+            Text("\(timeString(time: timeRemaining))")
+            .font(.largeTitle)
+        Button(action: {
+            if isRunning {
+                stop()
+            } else {
+                start()
             }
-        Text("PomoTimer!")
+        }) {
+            Text(isRunning ? "Pause" : "Start")
+                .font(.title)
+                .background(isRunning ? Color.red : Color.green)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+        }
+        Button("reset") {
+            reset()
+                    }
+
+
     }
+    private func timeString(time: Int) -> String {
+            let minutes = Int(time) / 60 % 60
+            let seconds = Int(time) % 60
+            return String(format:"%02i:%02i", minutes, seconds)
+        }
+    private func start() {
+        isRunning = true
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            } else {
+                stop()
+            }
+        }
+    }
+
+    private func stop() {
+        isRunning = false
+        timer?.invalidate()
+    }
+    private func reset(){
+        isRunning = false
+        timer?.invalidate()
+        timeRemaining = 0
+    }
+    
+
 }
 
 #Preview {
